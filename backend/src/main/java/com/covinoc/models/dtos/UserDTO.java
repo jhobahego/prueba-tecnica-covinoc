ad29e2cd8334ecb55c88b220188470492326836d
Jhon Hernandez <jhobahego16@hotmail.com>
2024-01-08 17:45:15 -0500

    Remove Campo innecesario

--- backend/src/main/java/com/covinoc/models/dtos/UserDTO.java
+++ backend/src/main/java/com/covinoc/models/dtos/UserDTO.java
@@ -3,5 +3,5 @@ package com.covinoc.models.dtos;
 import lombok.Builder;
 
 @Builder
-public record UserDTO(Long id, String name, String phoneNumber) {
+public record UserDTO(String name, String phoneNumber) {
 }
